f6b94b19e19f4bca00cd42296e1e09e2f1c1fcb5
Cristian Patricio Salinas Talamilla <ctalamilla@MBP-de-Cristian.silver.local>
2022-01-25 14:48:22 -0300

    practica terminada

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const path = require('path');
 
 const app = express();
 
-app.listen(3030, ()=> console.log('exitoso'));
+const pathPublic = path.resolve(__dirname, './public')
 
+app.use( express.static(pathPublic));
 
-//app.get('/', (req, res)=> res.send("Bienvenidos al sitio"));
+app.listen(3030, ()=> console.log('Mercado Liebre esta corriendo!'));
 
 app.get('/', (req,res)=> {
-    let htmlpath = path.resolve(__dirname, './views/index.html')
+    let htmlpath = path.resolve(__dirname, './views/home.html')
     res.sendFile(htmlpath)
 })
 
 
-app.use(express.static('public'));
+
